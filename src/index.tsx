84ecad34a5413cea29336dd990aa1228afda3aa6
Luciano Jardim <luciano.c.jardim@gmail.com>
2020-12-09 05:13:34 -0600

    initial setup

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <CssBaseline />
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>
+    </React.Fragment>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
